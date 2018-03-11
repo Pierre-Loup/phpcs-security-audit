e605d0371329c385f854589050aaa90d826e5813
Pierre-Loup Tristant <pierre-loup.tristant@niji.fr>
2018-03-11 21:42:33 +0100

    Fixed CI

--- Security/Tests/AbstractSniffUnitTest.php
+++ Security/Tests/AbstractSniffUnitTest.php
@@ -25,28 +25,6 @@ abstract class AbstractSniffUnitTest extends PHPCSAbstractSniffUnitTest
         $this->standardsDir = __DIR__.'/../';
 
     }//end setUp()
-
-
-    /**
-     * Returns the lines where errors should occur.
-     *
-     * The key of the array should represent the line number and the value
-     * should represent the number of errors that should occur on that line.
-     *
-     * @return array<int, int>
-     */
-    abstract protected function getErrorList();
-
-
-    /**
-     * Returns the lines where warnings should occur.
-     *
-     * The key of the array should represent the line number and the value
-     * should represent the number of warnings that should occur on that line.
-     *
-     * @return array<int, int>
-     */
-    abstract protected function getWarningList();
-
+    
 
 }//end class
