80aebed72eb5d6e72f1b2a62e3dd9d881ccc670d
Pierre-Loup Tristant <pierre-loup.tristant@niji.fr>
2018-03-13 22:25:10 +0100

    Added unit tests for AssertsSniff

---
+++ Security/Tests/BadFunctions/AssertsUnitTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Unit test class for the Asserts sniff.
+ *
+ * @license   https://github.com/FloeDesignTechnologies/phpcs-security-audit/blob/master/LICENSE GPLv3 
+ */
+
+namespace PHPCS_SecurityAudit\Tests\BadFunctions;
+
+use PHPCS_SecurityAudit\Tests\AbstractSniffUnitTest;
+
+class AssertsUnitTest extends AbstractSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList()
+    {
+        $errors = [
+            4 => 1
+        ];
+
+        return $errors;
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getWarningList()
+    {
+        $warnings = [
+            3 => 1
+        ];
+
+        return $warnings;
+
+    }//end getWarningList()
+
+
+}//end class
