515efe3f394b7cbf6c9dd311cc3b838ce16f656f
Pierre-Loup Tristant <pierre-loup.tristant@niji.fr>
2018-03-16 22:48:11 +0100

    Added unit tests for SQLFunctionsSniff ans SystemExecFunctionsSniff

---
+++ Security/Tests/BadFunctions/SQLFunctionsUnitTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Unit test class for the SQLFunctions sniff.
+ *
+ * @license   https://github.com/FloeDesignTechnologies/phpcs-security-audit/blob/master/LICENSE GPLv3 
+ */
+
+namespace PHPCS_SecurityAudit\Tests\BadFunctions;
+
+use PHPCS_SecurityAudit\Tests\AbstractSniffUnitTest;
+
+class SQLFunctionsUnitTest extends AbstractSniffUnitTest
+{
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList()
+    {  
+        return [
+            4 => 1
+        ];
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getWarningList()
+    {
+        return [
+            3 => 1
+        ];
+        
+    }//end getWarningList()
+
+
+}//end class
