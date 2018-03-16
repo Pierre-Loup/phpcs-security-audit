e013b9c2cd59cb9f133d35abb861a5a7d010eee4
Pierre-Loup Tristant <pierre-loup.tristant@niji.fr>
2018-03-16 22:16:03 +0100

    Added unit tests for PregReplaceSniff

---
+++ Security/Tests/BadFunctions/PregReplaceUnitTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Unit test class for the PregReplace sniff.
+ *
+ * @license   https://github.com/FloeDesignTechnologies/phpcs-security-audit/blob/master/LICENSE GPLv3 
+ */
+
+namespace PHPCS_SecurityAudit\Tests\BadFunctions;
+
+use PHPCS_SecurityAudit\Tests\AbstractSniffUnitTest;
+
+class PregReplaceUnitTest extends AbstractSniffUnitTest
+{
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList()
+    {  
+        return [
+            4 => 1,
+            5 => 1
+        ];
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getWarningList()
+    {
+        return [
+            3 => 1,
+            4 => 1,
+            6 => 1,
+            7 => 1
+        ];
+        
+    }//end getWarningList()
+
+
+}//end class
