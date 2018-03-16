e34ee36196d09065a1e43f2a39de50f9a17a1101
Pierre-Loup Tristant <pierre-loup.tristant@niji.fr>
2018-03-16 17:53:25 +0100

    Added unit tests for NoEvalsSniff

---
+++ Security/Tests/BadFunctions/NoEvalsUnitTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Unit test class for the NoEvals sniff.
+ *
+ * @license   https://github.com/FloeDesignTechnologies/phpcs-security-audit/blob/master/LICENSE GPLv3 
+ */
+
+namespace PHPCS_SecurityAudit\Tests\BadFunctions;
+
+use PHPCS_SecurityAudit\Tests\AbstractSniffUnitTest;
+
+class NoEvalsUnitTest extends AbstractSniffUnitTest
+{
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList()
+    {  
+        return [
+            3 => 1
+        ];
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getWarningList()
+    {
+        return [];
+        
+    }//end getWarningList()
+
+
+}//end class
